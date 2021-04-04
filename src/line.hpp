01b10bedc2898dc04993f2c8450eedf573b6457a
gvnnz <monocasual@gmail.com>
2021-04-04 14:25:31 +0200

    Add license header

--- src/line.hpp
+++ src/line.hpp
@@ -1,3 +1,29 @@
+/* -----------------------------------------------------------------------------
+ *
+ * geompp - Basic geometrical utilities for C++.
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Copyright (C) 2021 Giovanni A. Zuliani | Monocasual Laboratories
+ *
+ * This file is part of geompp - Basic geometrical utilities for C++.
+ *
+ * geompp - Basic geometrical utilities for C++ is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * geompp - Basic geometrical utilities for C++ is distributed in the hope that 
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with geompp - Basic geometrical utilities for C++. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------- */
+
 #ifndef GEOMPP_LINE_HH
 #define GEOMPP_LINE_HH
 
@@ -34,24 +60,19 @@ public:
 	{
 	}
 
-	/* withShiftedX
-    Returns a new Line horizontally shifted by a certain amount. */
-
-	Line withShiftedX(T delta) const
-	{
-		Line l = *this;
-		l.shiftX(delta);
-		return l;
-	}
-
 	/* shiftX
     Horizontally shifts the line of a certain amount. */
 
-	void shiftX(T delta)
+	void shiftX(T amount)
 	{
-		x1 += delta;
-		x2 += delta;
+		x1 += amount;
+		x2 += amount;
 	}
+
+	/* withShiftedX
+    Returns a new Line horizontally shifted by a certain amount. */
+
+	Line withShiftedX(T amount) const { return {x1 + amount, y1, x2 + amount, y2}; }
 
 	T x1, y1, x2, y2;
 
