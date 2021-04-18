f7f98d9e451bba0efe460b42329bb21b93ad0fdb
gvnnz <monocasual@gmail.com>
2021-04-18 21:43:39 +0200

    Removed wrong Line::withTrimmed[...] methods

--- src/line.hpp
+++ src/line.hpp
@@ -77,13 +77,6 @@ public:
 	Line withX2(T v) const { return {x1, y1, v, y2}; }
 	Line withY2(T v) const { return {x1, y1, x2, v}; }
 
-	/* withTrimmed[...]
-	Returns a copy of this Line with 'amount' removed from the one of the end
-	points. */
-
-	Line withTrimmedStart(T amount) const { return {x1 - amount, y1 - amount, x2, y2}; }
-	Line withTrimmedEnd(T amount) const { return {x1, y1, x2 - amount, y2 - amount}; }
-
 	/* withShiftedX
     Returns a new Line horizontally shifted by a certain amount. */
 
